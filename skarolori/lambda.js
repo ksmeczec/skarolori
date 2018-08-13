1f848117c498ce8b19d1ae44e1854d9d2772ba79
ksmeczec <42334286+ksmeczec@users.noreply.github.com>
2018-08-13 09:57:07 +0300

    adding sqs

--- skarolori/lambda.js
+++ skarolori/lambda.js
@@ -1,22 +1,21 @@
 let AWS = require('aws-sdk');
+let SL_AWS = require('slappforge-sdk-aws');
+const sqs = new SL_AWS.SQS(AWS);
 const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
 
+	sqs.sendMessage({
+		MessageBody: 'hi',
+		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/263248768798/skarolori',
+		DelaySeconds: '0',
+		MessageAttributes: {}
+	}, function (data) {
+		// your logic (logging etc) to handle successful message delivery, should be here
+	}, function (error) {
+		// your logic (logging etc) to handle failures, should be here
+	});
 
-	s3.getBucketLocation({
-		'Bucket': "com.sigma.test.263248768798.williesteyn.eu-west-2"
-	}).promise()
-		.then(data => {
-			console.log(data);           // successful response
-			/*
-			data = {
-				LocationConstraint: "us-west-2"
-			}
-			*/
-		})
-		.catch(err => {
-			console.log(err, err.stack); // an error occurred
-		});
+
 
 
 	callback(null, 'Successfully executed');
