799858216d186f76748f415260c749052c5bb328
ksmeczec <42334286+ksmeczec@users.noreply.github.com>
2018-08-13 09:30:45 +0300

    adding s3

--- skarolori/lambda.js
+++ skarolori/lambda.js
@@ -1,6 +1,23 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const s3 = new AWS.S3();
+exports.handler = function (event, context, callback) {
 
 
-	callback(null,'Successfully executed');
+	s3.getBucketLocation({
+		'Bucket': "com.sigma.test.263248768798.williesteyn.eu-west-2"
+	}).promise()
+		.then(data => {
+			console.log(data);           // successful response
+			/*
+			data = {
+				LocationConstraint: "us-west-2"
+			}
+			*/
+		})
+		.catch(err => {
+			console.log(err, err.stack); // an error occurred
+		});
+
+
+	callback(null, 'Successfully executed');
 }
